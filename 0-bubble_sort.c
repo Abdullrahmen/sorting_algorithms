7437ff33ee1de7010a2474e8b4845372a52867ad
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-10-22 22:15:52 +0300

    Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+#include <stdbool.h>
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+* bubble_sort - sort the array using bubble sort algorithm.
+* @array: the required array.
+* @size: the size of the array.
+*
+* Description: Prints the array after each swap.
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+		if (swapped == false)
+			break;
+	}
+}
